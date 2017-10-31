61370646e8538dcf3cf0840b3a4c95f3fa96e63a
DESKTOP-P3UJU5T\KP <kirk.pantin@triosstudent.com>
2017-10-31 14:23:14 -0400

    Yo

--- poolapp/poolapp/settings.py
+++ poolapp/poolapp/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = 'poolapp.wsgi.application'
 
 
@@ -117,6 +119,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "poolapp/static"),
+]
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
--- poolapp/poolapp/urls.py
+++ poolapp/poolapp/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
