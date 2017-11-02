5746da16717c2bebdab54d3a60f7b0cca6158bed
Sebi <Sebastian.crainic@hotmail.com>
2017-11-02 13:12:23 -0400

    commit rank changes #1

--- poolapp/billiards/views.py
+++ poolapp/billiards/views.py
@@ -1,25 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import transaction
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+from . import forms
+
 
 def index(request):
-    return render(request, 'poolapp/index.html')
+    current_user = request.user
+    user_id = current_user.id
+    user=User.objects.get(pk=user_id)
+    return render(request, 'poolapp/index.html',{'user':user})
     
-def update_profile(request, user_id):
-    user = User.objects.get(pk=user_id)
-    user.profile.rank = '2'
-    user.save()
-
+    
+@login_required
+@transaction.atomic
 
 def update_profile(request):
     if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
+        user_form = forms.UserForm(request.POST, instance=request.user)
         if user_form.is_valid():
             user_form.save()
-            messages.success(request, _('Your rank was successfully updated!'))
-            return redirect('settings:profile')
-        else:
-            messages.error(request, _('Please correct the error below.'))
+            
+            return redirect('index')
+        
+            
     else:
-        user_form = UserForm(instance=request.user)
-    return render(request, 'profiles/profile.html', {
+        user_form = forms.UserForm(instance=request.user)
+    return render(request, 'poolapp/EnterRank.html', {
         'user_form': user_form,
     })
